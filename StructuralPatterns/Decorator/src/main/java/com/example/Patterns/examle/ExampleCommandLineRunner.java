6f84fbd4bb5de93b2a50e08c3e06676a8d3c847b
fatherofsevenchildren <kudienko030303@mail.ru>
2025-05-11 20:59:30 +0300

    fix style

---
+++ StructuralPatterns/Decorator/src/main/java/com/example/Patterns/examle/ExampleCommandLineRunner.java
@@ -0,0 +1,16 @@
+package com.example.Patterns.examle;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ExampleCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
